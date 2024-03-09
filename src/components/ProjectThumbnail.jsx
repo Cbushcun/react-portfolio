76b00f4fb879e3e0803aaf440e9fef306cb09863
Christian Bush <bushc325@gmail.com>
2024-03-09 18:09:05 -0500

    fix project thumbnail

--- src/components/ProjectThumbnail.jsx
+++ src/components/ProjectThumbnail.jsx
@@ -2,12 +2,9 @@ function ProjectThumbnail({ handleClick, project }) {
   return (
     <button
       onClick={handleClick}
-      className="flex h-56 w-full max-w-64 flex-col gap-2 rounded-xl border-stone-200/40 bg-stone-200 bg-opacity-0 p-6 transition duration-150 hover:border-2 hover:bg-opacity-5 active:bg-opacity-10 "
+      className="flex h-56 w-full max-w-52 flex-col gap-2 rounded-xl border-stone-200/40 bg-stone-200 bg-opacity-0 px-3 py-6 transition duration-150 hover:border-2 hover:bg-opacity-5 active:bg-opacity-10 "
     >
-      <div
-        className="w-full flex-grow bg-cover bg-center"
-        style={{ backgroundImage: `url(${project.imgLink})` }}
-      />
+      <img className="m-auto bg-cover bg-center" src={`${project.imgLink}`} />
       <p className="text-center">{project.name}</p>
     </button>
   );
