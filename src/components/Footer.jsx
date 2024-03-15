e047331d131e9a11ec322f94dcbb2f9b4be2a5c0
Christian Bush <bushc325@gmail.com>
2024-03-15 16:13:41 -0400

    minor change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,35 +1,9 @@
-import { Link } from "react-scroll";
-
 function Footer() {
   return (
-    <footer className="just grid w-full gap-2 text-center text-stone-200 xl:grid-cols-3 xl:gap-0 xl:pb-5">
-      <Link
-        to="landing"
-        smooth={true}
-        className="w-fit items-center place-self-center text-xl hover:cursor-pointer hover:text-green-400 xl:col-start-2"
-      >
-        <i className="fa-solid fa-arrows-up-to-line w-full text-center"></i>
-        <p>Back To Top</p>
-      </Link>
-      <div className="xl:col-start-1">
-        Contact Me:{" "}
-        <a
-          href="mailto:chrizztianbush@gmail.com"
-          className="hover:text-green-400 hover:underline"
-        >
-          chrizztianbush@gmail.com
-        </a>{" "}
-        |{" "}
-        <a
-          href="https://www.linkedin.com/in/yourprofile"
-          className="hover:text-green-400 hover:underline"
-        >
-          LinkedIn
-        </a>
-      </div>
-      <div className="xl:col-start-3">
-        &copy; <span className="font-semibold">2024 Christian Bush</span>. All
-        rights reserved.
+    <footer className="flex w-full flex-col items-center bg-stone-950/50 py-5">
+      <div>Made with TailwindCSS</div>
+      <div>
+        &copy; <span>2024 Christian Bush</span>. All rights reserved.
       </div>
     </footer>
   );
