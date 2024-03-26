032239cf3fdf87fda7b9a586afebe5dcb5472e75
Christian Bush <bushc325@gmail.com>
2024-03-25 23:45:52 -0400

    text align fix

--- src/components/ProjectThumbnail.jsx
+++ src/components/ProjectThumbnail.jsx
@@ -19,7 +19,7 @@ function ProjectThumbnail({ handleClick, project }) {
       className="flex h-56 w-full max-w-52 flex-col gap-2 rounded-xl bg-stone-200 bg-opacity-0 px-3 outline-1 transition duration-150 hover:bg-opacity-5 hover:outline active:bg-opacity-10"
     >
       <img className="m-auto bg-cover bg-center" src={`${project.imgLink}`} />
-      <p className="text-center">{project.name}</p>
+      <p className="w-full text-center">{project.name}</p>
     </button>
   );
 }
